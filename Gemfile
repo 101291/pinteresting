c390a098c051259fcb9d2d65c8eeaba372317685
101291 <101291@users.noreply.github.com>
2015-03-31 23:58:51 +0000

    Added Amazon S3 for Paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'spring',        group: :development
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.4.1'
 gem 'paperclip', '~> 4.2'
+gem 'aws-sdk', '< 2.0'
 
 group :development, :test do
     gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  #Required for Heroku
+  # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'http://omr101291.herokuapp.com/' }
+  
+  # Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
